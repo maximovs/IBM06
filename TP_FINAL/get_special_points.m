f6bd7d3cc9c326da0c834085911936c27d332e8f
Máximo <maximovs@gmail.com>
2016-08-22 20:51:15 -0300

    TP final added

---
+++ TP_FINAL/get_special_points.m
@@ -0,0 +1,15 @@
+function [ loc_max, loc_min ] = get_special_points( signal )
+%Generates the vectors with the characteristic points.
+
+dsignal = diff(signal);
+[~, location] = findpeaks(dsignal, 'minpeakheight', 0.6*max(dsignal));
+T = median(diff(location));
+%% Se calculan los picos
+% Conociendo el T (período), calculo los picos con findpeaks.
+
+[~, loc_max] = findpeaks(signal, 'minpeakdistance', 0.9*T);
+[~, loc_min] = findpeaks(-signal, 'minpeakdistance', 0.9*T);
+
+
+end
+
---
+++ TP_FINAL/get_wk2_pp.m
@@ -0,0 +1,11 @@
+function [ pp ] = get_wk2_pp( avg_pao_beat, r, c, diastolic_start, fs, q_max, p_diastolic_end )
+%Get calculated
+%   Detailed explanation goes here
+t_s = 0:1/fs:(diastolic_start-1)/fs;
+t_d = (diastolic_start)/fs:1/fs:(length(avg_pao_beat)-1)/fs;
+p_sis = (p_diastolic_end - r*q_max)*exp(-t_s/(c*r)) + r*q_max;
+p_fs = p_sis(end);
+p_dia = p_fs*exp(-(t_d - t_d(1))/(c*r));
+pp = p_fs - p_diastolic_end;
+end
+
